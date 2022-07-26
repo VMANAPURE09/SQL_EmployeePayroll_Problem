b93e2e4b79de77a17b971920035102d21af6ea01
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-26 17:32:00 +0530

    UC2-Create Table Employee_Payroll

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -3,3 +3,13 @@ Create Database Employee_payroll_service
 
 --go to the databse
 use Employee_payroll_service
+
+--UC2-Creating the Table
+Create Table employee_payroll(
+id int primary key identity not null,
+name varchar(20) not null,
+salary int not null,
+startdate date not null)
+
+--to select all / display the table
+select * from employee_payroll
